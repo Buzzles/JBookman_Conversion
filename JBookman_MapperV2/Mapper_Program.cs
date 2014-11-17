f15e3f3050a242db68f572c9f6acd675ca7d74b5
Ian B <ibuswell@gmail.com>
2014-11-17 20:09:20 +0000

    Adding Mapper applications to the correct repo path

---
+++ JBookman_MapperV2/Mapper_Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.IO;
+using System.Runtime.Serialization;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.Drawing;
+using System.Drawing.Imaging;
+
+namespace JBookman_MapperV2
+{
+    class Mapper_Program :Form
+    {
+
+
+        //Application entry point.
+        [STAThread]
+        static void Main()
+        {
+            Mapper_Program newForm = new Mapper_Program();
+            Application.Run(newForm);
+        }
+    
+    }
+}
