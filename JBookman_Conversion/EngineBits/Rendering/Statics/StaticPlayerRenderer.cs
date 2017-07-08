5a9eae5f1788b3af3a14a76d5b70f7bd0d7dd5d3
Ian <ianbuswell@gmail.com>
2017-07-08 14:03:41 +0100

    Swap PlayerRenderer to translate grid first and draw player at 0.0 rather than try and draw offset

--- JBookman_Conversion/EngineBits/Rendering/Statics/StaticPlayerRenderer.cs
+++ JBookman_Conversion/EngineBits/Rendering/Statics/StaticPlayerRenderer.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using OpenTK.Graphics.OpenGL;
 using JBookman_Conversion.EngineBits.Rendering;
+using OpenTK;
 
 namespace JBookman_Conversion.EngineBits
 {
@@ -70,24 +71,49 @@ namespace JBookman_Conversion.EngineBits
             GL.BlendFunc(BlendingFactorSrc.SrcAlpha, BlendingFactorDest.OneMinusSrcAlpha);
             GL.LoadIdentity();
 
-            // TODO: TRANSLATE INSTEAD
+            //Proper GL way, translate grid, then draw at new 0.0
+            GL.PushMatrix();
+
+            // Matrices applied from right = last transform operation applied first!
+            var translateVector = new Vector3(playerPrimitive.X, -playerPrimitive.Y, playerPrimitive.Z);
+            GL.Translate(translateVector);
 
             GL.Begin(PrimitiveType.Quads);
+            ////quad1
+            ////bottomleft
+            //GL.TexCoord2(0, 0);
+            //GL.Vertex3(playerPrimitive.X, -(playerPrimitive.Y) - 1.0f, playerPrimitive.Z);
+            
+            ////top left
+            //GL.TexCoord2(0, 1);
+            //GL.Vertex3(playerPrimitive.X, -playerPrimitive.Y, playerPrimitive.Z);
+            ////top right
+            //GL.TexCoord2(1, 1);
+            //GL.Vertex3(playerPrimitive.X + 1.0f, -playerPrimitive.Y, playerPrimitive.Z);
+            ////bottom right
+            //GL.TexCoord2(1, 0);
+            //GL.Vertex3(playerPrimitive.X + 1.0f, -playerPrimitive.Y - 1.0f, playerPrimitive.Z);
+
+            // NEW
             //quad1
             //bottomleft
             GL.TexCoord2(0, 0);
-            GL.Vertex3(playerPrimitive.X, -(playerPrimitive.Y) - 1.0f, playerPrimitive.Z);
+            GL.Vertex3(0, -1.0f, 0.0f);  //vertex3(x,y,z)
             //top left
             GL.TexCoord2(0, 1);
-            GL.Vertex3(playerPrimitive.X, -playerPrimitive.Y, playerPrimitive.Z);
+            GL.Vertex3(0, 0.0f, 0.0f);
             //top right
             GL.TexCoord2(1, 1);
-            GL.Vertex3(playerPrimitive.X + 1.0f, -playerPrimitive.Y, playerPrimitive.Z);
+            GL.Vertex3(1.0f, 0.0f, 0.0f);
             //bottom right
             GL.TexCoord2(1, 0);
-            GL.Vertex3(playerPrimitive.X + 1.0f, -playerPrimitive.Y - 1.0f, playerPrimitive.Z);
+            GL.Vertex3(1.0f, -1.0f, 0.0f);
+
+
 
             GL.End();
+
+            GL.PopMatrix();
 
             GL.Disable(EnableCap.Blend);
         }
