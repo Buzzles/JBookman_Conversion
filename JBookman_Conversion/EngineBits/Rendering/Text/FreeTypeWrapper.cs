1391c07db92a8699151ce0930da755264bbe8652
Ian <ianbuswell@gmail.com>
2019-01-02 01:15:58 +0000

    Add in core of TextRenderer class to load a texture and build a character map. Start adding binds.

--- JBookman_Conversion/EngineBits/Rendering/Text/FreeTypeWrapper.cs
+++ JBookman_Conversion/EngineBits/Rendering/Text/FreeTypeWrapper.cs
@@ -1,10 +1,16 @@
-﻿using System.Runtime.InteropServices;
+﻿using System;
+using System.Runtime.InteropServices;
 
 namespace JBookman_Conversion.EngineBits.Rendering.Text
-{
+{    
     class FreeTypeWrapper
     {
-        [DllImport("freetype.dll")]
-        public static extern int FT_Init_FreeType(out System.IntPtr lib);
+        const string freetypePath = "libs/freetype.dll";
+
+        [DllImport(freetypePath)]
+        public static extern int FT_Init_FreeType(out IntPtr lib);
+
+        [DllImport(freetypePath)]
+        public static extern int FT_New_Face(IntPtr lib, string fontPath, int faceIndex, out IntPtr face);
     }
 }
